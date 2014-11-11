354a172490f0935126a0ddcdbedeac2cc9576949
Anwar A. Ruff <anwarruff@gmail.com>
2014-11-11 00:05:29 -0500

    Updated default init args to integers
    Removed string formatting of width and height

--- zmodulo/plot/rectangle/dimension.py
+++ zmodulo/plot/rectangle/dimension.py
@@ -1,10 +1,10 @@
 class Dimension:
     """ The Z-tree Dimension Container
     """
-    def __init__(self, height=10.00, width=10.00):
+    def __init__(self, width=10, height=10):
         self.height = height
         self.width = width
         self.template = "\twidth = {width};\n\theight = {height};\n"
 
     def to_str(self):
-        return self.template.format(width="{:.2f}".format(self.width), height="{:.2f}".format(self.height))
+        return self.template.format(width=self.width, height=self.height)

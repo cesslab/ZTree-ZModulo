a4d1e25152cb7edc3392d9e57948e32d14b41086
Anwar A. Ruff <anwarruff@gmail.com>
2014-11-11 00:02:23 -0500

    added function/class doc block

--- zmodulo/plot/properties/condition.py
+++ zmodulo/plot/properties/condition.py
@@ -1,14 +1,20 @@
 class Condition:
+    """ A Z-Tree Plot Condition """
 
     def __init__(self, predicate=""):
         """
-        :param predicate: z-tree plot entity display condition
+        :param predicate: z-tree plot predicate
         :type predicate: str
         """
         self.predicate = predicate
         self.template = '\tdisplaycondition = {predicate};\n'
 
     def to_str(self):
+        """
+        Converts the condition to a z-tree condition property declaration.
+        :return: Z-Tree condition property assignment
+        :rtype: str
+        """
         if not self.predicate:
             return ""
 

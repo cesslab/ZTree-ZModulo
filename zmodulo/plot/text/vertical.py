da73f3bd942a0ba7e329e7eaf6f1d8ceb544e958
Anwar A. Ruff <anwarruff@gmail.com>
2014-11-10 23:58:36 -0500

    renamed vertical alignment

--- zmodulo/plot/text/vertical.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Vertical(Enum):
-    """An enum containing the vertical alignment options for Z-Tree UI properties"""
-    center = "CENTER"
-    top = "TOP"
-    bottom = "BOTTOM"

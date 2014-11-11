4027e44d6538201559a7bc9a16780e8f298ef2ff
Anwar A. Ruff <anwarruff@gmail.com>
2014-11-11 00:00:49 -0500

    Updated class doc string

--- zmodulo/plot/line/end_point.py
+++ zmodulo/plot/line/end_point.py
@@ -2,12 +2,13 @@ from zmodulo.plot.properties.coordinate import Coordinate
 
 
 class EndPoint(Coordinate):
-    """ A plotline endpoint.
+    """ A Z-Tree plot line end point
     """
 
     def __init__(self, x, y):
         """
-        Initialize the plotline endpoint.
+        Initialize the plot line endpoint.
+
         :param x: x coordinate
         :type x: number
         :param y: y coordinate

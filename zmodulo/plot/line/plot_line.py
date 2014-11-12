7b76b3abb037d3d6d3449051b7756669c138e4ea
Anwar A. Ruff <anwarruff@gmail.com>
2014-11-11 20:40:54 -0500

    renamed init arg line to line_style

--- zmodulo/plot/line/plot_line.py
+++ zmodulo/plot/line/plot_line.py
@@ -1,6 +1,6 @@
 from zmodulo.plot.line.start_point import StartPoint
 from zmodulo.plot.properties.condition import Condition
-from zmodulo.plot.line.line import Line
+from zmodulo.plot.line.line_style import LineStyle
 
 
 class PlotLine:
@@ -8,14 +8,14 @@ class PlotLine:
     """
 
     def __init__(self, start_point, end_point,
-                 line=None, condition=None, element_id=""):
+                 line_style=None, condition=None, element_id=""):
         """
         :param start_point: plot line left coordinate
         :type start_point: StartPoint
         :param end_point: plot line right coordinate
         :type end_point: EndPoint
-        :param line: line style
-        :type line: Line
+        :param line_style: line style
+        :type line_style: Line
         :param condition: z-tree condition string
         :type condition: str
         :param element_id: z-tree plot entity ID
@@ -24,10 +24,10 @@ class PlotLine:
         self.start_point = start_point
         self.end_point = end_point
 
-        if line is None:
-            self.line = Line()
+        if line_style is None:
+            self.line = LineStyle()
         else:
-            self.line = line
+            self.line = line_style
 
         if condition is None:
             self.condition = Condition()
@@ -36,7 +36,7 @@ class PlotLine:
 
         self.element_id = element_id
 
-        self.template = 'plotline "{id}"{{\n{body}}}\n'
+        self.template = 'plotline "{id}"{{\n{body}}}'
 
     def to_str(self):
         """

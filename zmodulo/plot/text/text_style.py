d2dc9591c2402700ee3a2aa2d1f57f3961976259
Anwar A. Ruff <anwarruff@gmail.com>
2014-11-11 20:38:50 -0500

    enable initialization of template with primitive types

--- zmodulo/plot/text/text_style.py
+++ zmodulo/plot/text/text_style.py
@@ -8,50 +8,50 @@ class TextStyle:
     """ The plot text style template
     """
 
-    def __init__(self, color=None, weight=None, size=None, font_type=None):
+    def __init__(self, text_color=None, font_size=None, font_weight=None, font_type=None):
         """
-        :param color: text color
-        :type color: TextColor
-        :param weight: text weight
-        :type weight:
-        :param size:
-        :type size:
+        :param text_color: text color
+        :type text_color: TextColor
+        :param font_size:
+        :type font_size:
+        :param font_weight: text weight
+        :type font_weight:
         :param font_type:
         :type font_type:
         :return:
         :rtype:
         """
-        if color is None:
-            self.color = TextColor()
+        if text_color is None:
+            self.text_color = TextColor()
         else:
-            self.color = color
+            self.text_color = text_color
 
         if font_type is None:
             self.font_type = FontType()
         else:
             self.font_type = font_type
 
-        if weight is None:
-            self.weight = FontWeight()
+        if font_weight is None:
+            self.font_weight = FontWeight()
         else:
-            self.weight = weight
+            self.font_weight = font_weight
 
-        if size is None:
-            self.size = FontSize()
+        if font_size is None:
+            self.font_size = FontSize()
         else:
-            self.size = size
+            self.font_size = font_size
 
-        self.template = '{color}{font_type}{weight}{size}'
+        self.template = '{text_color}{font_type}{font_weight}{font_size}'
 
     def to_str(self):
         """
-        Converts the plot text font weight instance to a z-tree plot text weight property declaration.
+        Converts the plot text style instance to a z-tree plot text style property declaration set.
         :return: plot text weight property declaration
         :rtype: str
         """
         return self.template.format(
-            color=self.color.to_str(),
+            text_color=self.text_color.to_str(),
             font_type=self.font_type.to_str(),
-            weight=self.weight.to_str(),
-            size=self.size.to_str()
+            font_weight=self.font_weight.to_str(),
+            font_size=self.font_size.to_str()
         )

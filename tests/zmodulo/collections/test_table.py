17d20d5daa6792806e5edac48098b9bd1e5fc76f
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-10 23:41:00 -0500

    Updated tests

--- tests/zmodulo/collections/test_table.py
+++ tests/zmodulo/collections/test_table.py
@@ -8,5 +8,5 @@ __author__ = 'aruff'
 class TestTable(TestCase):
     def test_find(self):
         table = Table("TestTable")
-        self.assertEqual(table.find("Same(Subject)", "someVar"),
-                         'TestTable.find(Same(Subject), someVar)')
+        self.assertEqual(table.find('someVar'), 'TestTable.find(someVar)')
+        self.assertEqual(table.filter_find(table.same('Subject'), 'someVar'), 'TestTable.find(Same(Subject), someVar)')

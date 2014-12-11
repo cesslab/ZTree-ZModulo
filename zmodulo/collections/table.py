0a3d0be3c47ea7ca61f0ac17b4088ec42068e219
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-10 23:31:48 -0500

    Added filter find

--- zmodulo/collections/table.py
+++ zmodulo/collections/table.py
@@ -14,7 +14,7 @@ class Table:
         self.same_template = "Same({})"
         self.str = ""
 
-    def find(self, find_filter, find_var):
+    def filter_find(self, find_filter, find_var):
         """
         :param find_filter: z-tree filter
         :type find_filter: str
@@ -23,8 +23,18 @@ class Table:
         :return: find string
         :rtype: str
         """
-        find_template = self.name + '.find({f}, {v})'
-        return find_template.format(f=find_filter, v=find_var)
+        find_template = self.name + self.find_template.format('{}, {}')
+        return find_template.format(find_filter, find_var)
+
+    def find(self, find_var):
+        """
+        :param find_var: z-tree var to find in the table
+        :type find_var: str
+        :return: find string
+        :rtype: str
+        """
+        find_template = self.name + '.find({v})'
+        return find_template.format(v=find_var)
 
     def same(self, var_name):
         return self.same_template.format(var_name)

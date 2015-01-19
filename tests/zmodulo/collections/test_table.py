0438fb62d860066b49d93ea7a06b4b79a0d09490
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-18 23:56:42 -0500

    Updated the table test

--- tests/zmodulo/collections/test_table.py
+++ tests/zmodulo/collections/test_table.py
@@ -8,5 +8,7 @@ __author__ = 'aruff'
 class TestTable(TestCase):
     def test_find(self):
         table = Table("TestTable")
+        self.assertEqual("TestTable.find(Table("TestTable").same('Subject').filter('Round', '==', '2').find('var'))
         self.assertEqual(table.find('someVar'), 'TestTable.find(someVar)')
-        self.assertEqual(table.filter_find(table.same('Subject'), 'someVar'), 'TestTable.find(Same(Subject), someVar)')
+        self.assertEqual(table.filtered_find('p == q & foo = bar', 'someVar'), 'TestTable.find(Same(Subject), someVar)')
+        self.assertEqual(table.filtered_find_same(table.same('Subject'), 'someVar'), 'TestTable.find(Same(Subject), someVar)')

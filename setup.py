53ae0b1e07c2b13cb08f7d803623010f508ba1b7
Anwar A. Ruff <anwarruff@gmail.com>
2015-04-18 14:23:01 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='zmodulo',
+    version='1.0.0',
+    packages=['zmodulo'],
+    url='http://github.com/aaruff/Zmodulo',
+    license='Apache',
+    author='Anwar A. Ruff',
+    author_email='',
+    description='Z-Tree GUI generators',
+)
